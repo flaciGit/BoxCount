a956b949aa92e89dbbdb5804938289add6e9d282
flaciGit <file.laszlo.n@gmail.com>
2019-10-02 19:00:05 +0200

    Update Program.cs

--- BoxCount/Program.cs
+++ BoxCount/Program.cs
@@ -6,16 +6,75 @@ using System.Threading.Tasks;
 
 namespace BoxCount
 {
-    class Program
+    class BoxCounter
     {
+        static int largeSize;
+        static int smallSize;
+
+        public BoxCounter(int large, int small) {
+            largeSize = large;
+            smallSize = small;
+        }
+
+        public static int CountBoxes(int items, int largeBoxes, int smallBoxes) {
+
+            Console.WriteLine("Item number: " + items);
+            Console.WriteLine("Available large boxes: " + largeBoxes);
+            Console.WriteLine("Available small boxes: " + smallBoxes);
+            
+            int largeBoxesNeeded = 0;
+            int smallBoxesNeeded = 0;
+
+            if(largeBoxes > 0)
+            {
+                for (int i = 1; i <= largeBoxes; i++)
+                {
+                    if (i * largeSize <= items)
+                        largeBoxesNeeded = i;
+                }
+            }
+            
+            if(largeBoxesNeeded * largeSize < items)
+            {
+                if (smallBoxes > 0)
+                {
+                    for (int i = 1; i <= smallBoxes; i++)
+                    {
+                        if (i * smallSize < items - largeBoxesNeeded * largeSize + smallSize)
+                            smallBoxesNeeded = i;
+                    }
+                }
+                
+            }
+
+            //Did the items fit in the available boxes?
+            if(largeBoxesNeeded * largeSize + smallBoxesNeeded * smallSize < items)
+            {
+                Console.WriteLine("The items did not fit in the available boxes!");
+
+            }
+            else
+            {
+                Console.WriteLine("Large boxes needed: " + largeBoxesNeeded);
+                Console.WriteLine("    Size of a large box: " + largeSize);
+                
+                Console.WriteLine("Small boxes needed: " + smallBoxesNeeded);
+                Console.WriteLine("    Size of a small box: " + smallSize);
+                
+                Console.WriteLine("Boxes needed overall: " + (largeBoxesNeeded + smallBoxesNeeded));
+            }
+
+            return 0;
+        }
+        
         static void Main(string[] args)
         {
-            // The code provided will print ‘Hello World’ to the console.
-            // Press Ctrl+F5 (or go to Debug > Start Without Debugging) to run your app.
-            Console.WriteLine("Hello World!");
-            Console.ReadKey();
+            BoxCounter myBoxCounter = new BoxCounter(5,2);
 
-            // Go to http://aka.ms/dotnet-get-started-console to continue learning how to build a console app! 
+            CountBoxes(21, 4, 2);
+            
+            Console.ReadKey();
+            
         }
     }
 }
